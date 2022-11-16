61800a98405f7f1d26bea6af361f01b9075e1c80
Eslam Alaa <zoro12zx@gmail.com>
2022-11-16 23:41:08 +0200

    Eslam Alaa - Patients: showPatients,save button functions.

--- Patients.cs
+++ Patients.cs
@@ -17,14 +17,34 @@ namespace Health_Care_Center_Management_System_Task
         {
             InitializeComponent();
             Con = new Functions();
-            
+            ShowPatients();
         }
 
-        
+        private void ShowPatients()
+        {
+            String Query = "Select * from PatientTable";
+            PatientListGV.DataSource = Con.GetData(Query);
+        }
 
         private void SaveBTN_Click(object sender, EventArgs e)
         {
-            
+            if(PatientNameTB.Text == "" || PatientGenderCB.SelectedIndex == -1 || PatientPhoneTB.Text == "" || PatientAddressTB.Text == "")
+            {
+                MessageBox.Show("Missing Data!!!");
+            }
+            else
+            {
+                String name = PatientNameTB.Text;
+                String gender = PatientGenderCB.SelectedItem.ToString();
+                String birthDate = PatientBirthDateDTP.Value.Date.ToString();
+                String phone = PatientPhoneTB.Text;
+                String address = PatientAddressTB.Text;
+                String Query = "insert into PatientTable values('{0}','{1}','{2}','{3}','{4}')";
+                Query = string.Format(Query, name, gender, birthDate, phone, address);
+                Con.SetData(Query);
+                ShowPatients();
+                MessageBox.Show("Patient Added!!!");
+            }
         }
     }
 }
